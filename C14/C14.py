4f1f934e713a7b2f35aa7dea2512a626efafc5b8
jan-tdy <janikdubovsky@gmail.com>
2025-04-18 00:27:28 +0200

    Update C14.py

--- C14/C14.py
+++ C14/C14.py
@@ -293,7 +293,7 @@ class SplashScreen(QtWidgets.QSplashScreen):
         for i in range(101):
             self.pr.setValue(i)
             QtWidgets.qApp.processEvents()
-            time.sleep(0.02)
+            time.sleep(0.010)
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
