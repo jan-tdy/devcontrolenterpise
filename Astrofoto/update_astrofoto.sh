accae7ca7168311d0ca8d2b3573c390f051d4313
jan-tdy <janikdubovsky@gmail.com>
2025-04-18 14:30:50 +0200

    Update update_astrofoto.sh
    now it's for astrofoto

--- Astrofoto/update_astrofoto.sh
+++ Astrofoto/update_astrofoto.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-# Skript pre aktualizáciu programu C14 z GitHubu
+# Skript pre aktualizáciu programu Astrofoto z GitHubu
 
 # Zastaví bežiaci program C14 (ak beží)
-pkill -f "python3 /home/dpv/j44softapps-socketcontrol/C14.py"
+pkill -f "python3 /home/dpv/j44softapps-socketcontrol/astrofoto.py"
 
 # Prejde do adresára s programom
 cd /home/dpv/j44softapps-socketcontrol/
 
 # Stiahne najnovšiu verziu programu z GitHubu
-curl -O https://raw.githubusercontent.com/jan-tdy/devcontrolenterpise/main/C14/C14.py
+curl -O https://raw.githubusercontent.com/jan-tdy/devcontrolenterpise/main/Astrofoto/Astrofoto.py
 
 # Overí, či sa stiahnutie úspešne dokončilo
 if [ $? -eq 0 ]; then
@@ -18,8 +18,8 @@ else
   exit 1
 fi
 
-# Reštartuje program C14
-python3 /home/dpv/j44softapps-socketcontrol/C14.py &
+# Reštartuje program Astrofoto
+python3 /home/dpv/j44softapps-socketcontrol/Astrofoto.py &
 
-echo "Program C14 bol reštartovaný."
+echo "Program Astrofoto bol reštartovaný."
 exit 0
