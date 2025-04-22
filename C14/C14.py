f917e75117c48ae144bf97b63745977530eda90c
jan-tdy <janikdubovsky@gmail.com>
2025-04-22 17:54:56 +0200

    Update C14.py

--- C14/C14.py
+++ C14/C14.py
@@ -60,18 +60,17 @@ class MainWindow(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Ovládanie Hvezdárne - C14")
-        screen_geometry = QtWidgets.QApplication.desktop().availableGeometry()
-        screen_width = screen_geometry.width()
-        screen_height = screen_geometry.height()
+        screen = QtWidgets.QApplication.primaryScreen().availableGeometry()
+        screen_width = screen.width()
+        screen_height = screen.height()
         
-        # Nastav rozumné rozmery: šírka 90% obrazovky, výška max 80%
         window_width = int(screen_width * 0.9)
-        window_height = int(screen_height * 0.8)
+        window_height = int(screen_height * 0.5)
         
         self.resize(window_width, window_height)
         self.move(
-            (screen_width - window_width) // 2,
-            (screen_height - window_height) // 2
+            screen.left() + (screen_width - window_width) // 2,
+            screen.top() + (screen_height - window_height) // 2
         )
 
         self.main_layout = QtWidgets.QWidget()
